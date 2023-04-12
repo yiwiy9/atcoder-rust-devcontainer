2727af72e3637372512701828221ebc1288b9a31
yiwiy9 <yiwiy9@gmail.com>
2023-04-13 01:22:30 +0900

    Fix entrypoint.sh for signing in automatically

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Create VS Code settings file for rust-analyzer if it doesn't exist
 settings_json=".vscode/settings.json"
 example_json=".vscode/settings.exapmle.json"
-
 if [ ! -f "$settings_json" ]; then
     cp "$example_json" "$settings_json"
 fi
+
+# Sign in automatically for Atcoder website
+cargo compete login atcoder <<EOF
+$ATCODER_USERNAME
+$ATCODER_PASSWORD
+EOF
