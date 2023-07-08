a7f73e90bd534101148f3d370698d6dca55e5d57
yiwiy9 <yiwiy9@gmail.com>
2023-07-08 17:07:57 +0900

    Fix `cargo_compete_new.sh` to output error

--- .devcontainer/cargo_compete_new.sh
+++ .devcontainer/cargo_compete_new.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Script requires exactly one argument
 if [ "$#" -ne 1 ]; then
   echo "Error: Exactly one argument required."
   echo "Usage: cargo_compete_new.sh <contest>"
@@ -9,22 +10,29 @@ fi
 # Change the working directory to the workspace folder
 cd $WORKSPACE_FOLDER
 
-# Execute 'cargo compete new'
-if ! cargo compete new $1; then
+# Set variables
+CONTEST=$1
+SETTINGS_JSON_PATH="${WORKSPACE_FOLDER}/.vscode/settings.json"
+CARGO_TOML_PATH="./src/contest/${CONTEST}/Cargo.toml"
+
+# Function to print jq error
+print_jq_error() {
+  echo "Error: Invalid or unexpected data in settings.json" >&2
+  jq "$1" $SETTINGS_JSON_PATH
   exit 1
-fi
+}
 
-# Get the relative path to Cargo.toml
-cargo_toml_path="./src/contest/$1/Cargo.toml"
-
-# If settings.json file exists, add the path to rust-analyzer.linkedProjects
-settings_json_path="./.vscode/settings.json"
+# Test if the settings.json is a valid JSON
+jq empty $SETTINGS_JSON_PATH >/dev/null 2>&1 || print_jq_error empty
 
 # Get the current contents of rust-analyzer.linkedProjects
-current_linked_projects=$(jq '.["rust-analyzer.linkedProjects"]' $settings_json_path)
+CURRENT_LINKED_PROJECTS=$(jq '.["rust-analyzer.linkedProjects"]' $SETTINGS_JSON_PATH) || print_jq_error '.["rust-analyzer.linkedProjects"]'
+
+# Execute 'cargo compete new'
+cargo compete new $CONTEST || exit 1
 
 # Add the new path
-updated_linked_projects=$(echo $current_linked_projects | jq ". + [\"$cargo_toml_path\"]")
+UPDATED_LINKED_PROJECTS=$(echo $CURRENT_LINKED_PROJECTS | jq ". + [\"$CARGO_TOML_PATH\"]")
 
 # Update settings.json
-jq ". * {\"rust-analyzer.linkedProjects\": $updated_linked_projects}" $settings_json_path > temp_settings.json && mv temp_settings.json $settings_json_path
+jq ". * {\"rust-analyzer.linkedProjects\": $UPDATED_LINKED_PROJECTS}" $SETTINGS_JSON_PATH > temp_settings.json && mv temp_settings.json $SETTINGS_JSON_PATH || print_jq_error ". * {\"rust-analyzer.linkedProjects\": $UPDATED_LINKED_PROJECTS}"
